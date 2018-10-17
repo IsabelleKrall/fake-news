12869f33b43be40d698b0edc52380d28c566e11e
Isabelle Krall <isabellekrall@Isabelles-MacBook-Pro.local>
2018-10-17 11:14:11 +0200

    added .editorconfig

--- data.php
+++ data.php
@@ -5,15 +5,35 @@ $data = [
   [
     'title'=> 'Lorem',
     'content' => 'Ipsum',
-    'picture' => 'img/03.png',
+    'picture' => 'img/01.png',
     'author' =>
         ['id' => 1,
-        'name' => 'Isabelle',
+        'name' => 'Isabelle Krall',
         ],
     'publishedDate' => date('Y'),
     'likeCounter' => 6,
   ],
+  [
+    'title'=> 'Lorem',
+    'content' => 'Ipsum',
+    'picture' => 'img/02.png',
+    'author' =>
+        ['id' => 2,
+        'name' => 'Anna Svensson',
+        ],
+    'publishedDate' => date('Y'),
+    'likeCounter' => 3,
+  ],
+  [
+    'title'=> 'Lorem',
+    'content' => 'Ipsum',
+    'picture' => 'img/03.png',
+    'author' =>
+        ['id' => 3,
+        'name' => 'Henrik Larsson',
+        ],
+    'publishedDate' => date('Y'),
+    'likeCounter' => 12,
+  ],
 ];
-
-
 print_r($data);
--- functions.php
+++ functions.php
@@ -2,5 +2,3 @@
 declare(strict_types=1);
 
 $content = file_get_contents(__DIR__.'content.txt');
-
-$data =
