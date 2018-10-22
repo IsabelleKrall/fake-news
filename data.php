9d3bb9edcd66387cdf9b0bd16037e4e6e5398731
Isabelle Krall <isabellekrall@Isabelles-MacBook-Pro.local>
2018-10-22 14:43:30 +0200

    added 'publishedDate' to index.php

--- data.php
+++ data.php
@@ -1,39 +1,67 @@
 <?php
 declare(strict_types=1);
+require __DIR__.'/functions.php';
 
-$data = [
+$authors = [
+	['id' => 1,
+	 'name' => 'Isabelle Krall',
+ 	],
+	['id' => 2,
+	 'name' => 'Silvia Bernadotte',
+ 	],
+	['id' => 3,
+	 'name' => 'Agneta Fältskog',
+	],
+	['id' => 4,
+	 'name' => 'Benny Andersson',
+	],
+	['id' => 5,
+	 'name' => 'Gustav Carlsson',
+	],
+];
+
+
+
+
+$news = [
   [
-    'title'=> 'Lorem',
-    'content' => 'Ipsum',
-    'picture' => 'img/01.png',
-    'author' =>
-        ['id' => 1,
-        'name' => 'Isabelle Krall',
-        ],
-    'publishedDate' => date('Y'),
+    'title'=> 'This is Bruce',
+    'content' => file_get_contents('content1.txt'),
+    'picture' => 'pictures/1.jpeg',
+	 	'author' => 1,
+    'publishedDate' => date("F j, Y, g:i a"),
     'likeCounter' => 6,
   ],
   [
     'title'=> 'Lorem',
-    'content' => 'Ipsum',
+    'content' => file_get_contents('content2.txt'),
     'picture' => 'img/02.png',
-    'author' =>
-        ['id' => 2,
-        'name' => 'Anna Svensson',
-        ],
-    'publishedDate' => date('Y'),
+    'author' => 2,
+    'publishedDate' => date("F j, Y, g:i a"),
     'likeCounter' => 3,
   ],
   [
     'title'=> 'Lorem',
-    'content' => 'Ipsum',
+    'content' => file_get_contents('content3.txt'),
     'picture' => 'img/03.png',
-    'author' =>
-        ['id' => 3,
-        'name' => 'Henrik Larsson',
-        ],
-    'publishedDate' => date('Y'),
+    'author' => 3,
+    'publishedDate' => date("F j, Y, g:i a"),
     'likeCounter' => 12,
   ],
+  [
+    'title'=> 'Lorem',
+    'content' => file_get_contents('content4.txt'),
+    'picture' => 'img/04.png',
+    'author' => 4,
+    'publishedDate' => date("F j, Y, g:i a"),
+    'likeCounter' => 20,
+  ],
+  [
+	 'title'=> 'Lorem',
+	 'content' => file_get_contents('content5.txt'),
+	 'picture' => 'img/05.png',
+	 'author' => 5,
+	 'publishedDate' => date("F j, Y, g:i a"),
+	 'likeCounter' => 9,
+  ],
 ];
-print_r($data);
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+require __DIR__.'/data.php';
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
@@ -9,8 +10,68 @@ declare(strict_types=1);
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title></title>
+		  <link rel="stylesheet" href="CSS/bootswatch.css">
+		   <link rel="stylesheet" href="style.css">
+		  <title></title>
     </head>
     <body>
+		 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Fake News</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+	 <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+	 <ul class="navbar-nav mr-auto">
+	   <li class="nav-item active">
+		  <a class="nav-link" href="#">Newsfeed <span class="sr-only">(current)</span></a>
+	   </li>
+	   <li class="nav-item">
+		  <a class="nav-link" href="#">Authors</a>
+	  </li>
+	   <li class="nav-item">
+		  <a class="nav-link" href="#">About</a>
+	   </li>
+	 </ul>
+  </div>
+</nav>
+
+	<main class="mt-4">
+		<div class="container">
+			<div class="row">
+				<?php foreach ($news as $post): ?>
+					<div class="col-10">
+						<div class="card text-white bg-primary mb-3" style="max-width: 50rem;">
+						  <div class="card-header">
+							  <?php foreach ($authors as $author): ?>
+								<?php if ($author['id'] === $post['author']): ?>
+								<h4>Author: <?php echo $author['name'] ?></h4>
+								<?php endif; ?>
+								<?php endforeach; ?>
+
+							</div>
+
+
+						  <div class="card-body">
+							 <div class="img"><?php echo $post['picture'] ?>
+
+							 </div>
+
+							  <img style="height: 300px; width: 100%; display: block;" src="<?php echo $post['picture'] ?>">
+							 <h4 class="card-title"><?php echo $post['title'] ?></h4>
+							 <p class="card-text"><?php echo $post['content'] ?></p>
+							 <p>Written: <?php echo $post['publishedDate'] ?></p>
+
+						  </div>
+					  </div>
+
+				  </div><!--<div class="col-6">-->
+
+					  <?php endforeach; ?>
+
+			</div>
+		</div>
+	</main>
+
     </body>
 </html>
