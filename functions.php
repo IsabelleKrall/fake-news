4c6046a774a767df2f807590b046813020c73074
Isabelle Krall <isabellekrall@Isabelles-MacBook-Pro.local>
2018-10-29 12:23:10 +0100

    added function: sortByDate + findArticlesById

--- functions.php
+++ functions.php
@@ -1,2 +1,12 @@
 <?php
 declare(strict_types=1);
+
+function sortByDate($a, $b) {
+ return strcmp($a['date'], $b['date']);
+}
+
+
+function findArticlesById(array $news): bool {
+	$URLAuthorId = $_GET['id'];
+	return $news['author'] == $URLAuthorId;
+}
