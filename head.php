d51f13bbf409ed89acf36d651cdb89bd4166e8e1
Isabelle Krall <isabellekrall@Isabelles-MacBook-Pro.local>
2018-10-29 12:23:44 +0100

    added head.php

---
+++ head.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+require __DIR__.'/data.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		  <link rel="stylesheet" href="CSS/bootswatch.css">
+		   <link rel="stylesheet" href="CSS/style.css">
+		  <title>Fake News by Fake Authors</title>
+    </head>
+    <body>
+		 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="index.php">Fake News</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+	 <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+	 <ul class="navbar-nav mr-auto">
+	   <li class="nav-item">
+		  <a class="nav-link" href="#">Authors</a>
+	  </li>
+	   <li class="nav-item">
+		  <a class="nav-link" href="about.php">About</a>
+	   </li>
+	 </ul>
+  </div>
+</nav>
