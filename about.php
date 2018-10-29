14824b339c8f6a91959965e822da254bf4d3d2a3
Isabelle Krall <isabellekrall@Isabelles-MacBook-Pro.local>
2018-10-29 12:23:32 +0100

    added about.php + content

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+require __DIR__.'/head.php';
+
+?>
+
+<div class="jumbotron jumbotronBox">
+  <h1 class="display-3">About us</h1>
+  <p class="lead">Fake News is a site where you can find fake news articles, written by fictional authors.</p>
+  <hr class="my-4">
+  <p>Feel free to read and comment on our articles! We love fake comments as much as we adore fake news.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="index.php" role="button">Read articles</a>
+  </p>
+</div>
