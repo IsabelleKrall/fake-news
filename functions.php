50ab8ca70f1eef327bf267be4c5cbf290e984174
Isabelle Krall <isabellekrall@Isabelles-MacBook-Pro.local>
2018-10-29 12:55:28 +0100

    added docblock

--- functions.php
+++ functions.php
@@ -1,11 +1,21 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * [sortByDate description]
+ * @param  array $a [description]
+ * @param  array $b [description]
+ * @return int      [description]
+ */
 function sortByDate(array $a, array $b): int {
  return strcmp($a['date'], $b['date']);
 }
 
-
+/**
+ * [findArticlesById description]
+ * @param  array $news [description]
+ * @return bool        [description]
+ */
 function findArticlesById(array $news): bool {
 	$URLAuthorId = $_GET['id'];
 	return $news['author'] == $URLAuthorId;
