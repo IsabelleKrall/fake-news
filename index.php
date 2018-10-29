b3a10803fba8d8eac8383512ff45a0e79eb18887
Isabelle Krall <isabellekrall@Isabelles-MacBook-Pro.local>
2018-10-29 12:24:56 +0100

    added comments-field + likeCounter

--- index.php
+++ index.php
@@ -1,73 +1,76 @@
-<?php
-declare(strict_types=1);
-require __DIR__.'/data.php';
-// This is the file where you can keep your HTML markup. We should always try to
-// keep us much logic out of the HTML as possible. Put the PHP logic in the top
-// of the files containing HTML or even better; in another PHP file altogether.
+<?php require __DIR__.'/head.php';
+
+if(isset($_POST ['name'], $_POST['message'], $_POST['author'])) {
+	$name = $_POST['name'];
+	$message = $_POST['message'];
+	$author = (int)$_POST['author'];
+	$newComment = ['name' => $name, 'message' => $message, 'author' => $author];
+	array_push($comments, $newComment);
+}
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		  <link rel="stylesheet" href="CSS/bootswatch.css">
-		   <link rel="stylesheet" href="CSS/style.css">
-		  <title></title>
-    </head>
-    <body>
-		 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Fake News</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-	 <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarColor01">
-	 <ul class="navbar-nav mr-auto">
-	   <li class="nav-item active">
-		  <a class="nav-link" href="#">Newsfeed <span class="sr-only">(current)</span></a>
-	   </li>
-	   <li class="nav-item">
-		  <a class="nav-link" href="#">Authors</a>
-	  </li>
-	   <li class="nav-item">
-		  <a class="nav-link" href="#">About</a>
-	   </li>
-	 </ul>
-  </div>
-</nav>
-
-	<main class="mt-4">
-		<div class="container">
-			<div class="row">
-				<?php foreach ($news as $post): ?>
-					<div class="col-10">
-						<div class="card text-white bg-primary mb-3" style="max-width: 50rem;">
-						  <div class="card-header">
-							  <?php foreach ($authors as $author): ?>
+<main class="mt-4">
+	<div class="container">
+		<div class="row mainContainer">
+			<?php usort($news,'sortByDate');?>
+			<?php foreach ($news as $post): ?>
+				<div class="col-10 mainContainer">
+					<div class="card text-white bg-primary mb-3" style="max-width: 50rem;">
+						<div class="card-header">
+							<?php foreach ($authors as $author): ?>
 								<?php if ($author['id'] === $post['author']): ?>
-								<h4>Author: <?php echo $author['name'] ?></h4>
+									<h4>Author: <a id="link" href="authorPosts.php?id=<?php echo $author['id'];?>">
+										<?php echo $author['name'] ?></h4>
+									</a>
 								<?php endif; ?>
-								<?php endforeach; ?>
-							</div>
+							<?php endforeach; ?>
+						</div>
 
-						  <div class="card-body">
+						<div class="card-body">
+							<img class="img" src="<?php echo $post['picture'] ?>">
+							<h4 class="card-title"><?php echo $post['title'] ?></h4>
+							<p class="card-text"><?php echo $post['content'] ?></p>
+							<p>Written: <?php echo $post['date'] ?></p>
+							<button type="button" class="btn btn-secondary">Likes: <?php echo $post['likeCounter']?></button></br></br>
 
 
-							 <img class="img" src="<?php echo $post['picture'] ?>">
-							 <h4 class="card-title"><?php echo $post['title'] ?></h4>
-							 <p class="card-text"><?php echo $post['content'] ?></p>
-							 <p>Written: <?php echo $post['publishedDate'] ?></p>
+							<div class="card-header">
+							<h4 class="card-title">Comments</h4>
+						<?php foreach ($comments as $comment): ?>
+							<?php if ($post['author'] === $comment['author']): ?>
 
-						  </div>
-					  </div>
+								<b><?php echo $comment['name'] ?>:</b></br>
+								<h6><?php echo $comment['message'] ?></h6></br>
+							<?php endif; ?>
+						<?php endforeach ?>
 
-				  </div><!--<div class="col-6">-->
+						<!--commentsBox-->
+						<div class="commentRow">
+							<form class="col-lg-6 offset-lg-3" action="index.php" method="post">
+								<hr>
+								<div class="form-group">
+									<label for="name">Name</label>
+									<input type="text" name="name" id="name" class="form-control">
+								</div><!-- /form-group -->
 
-					  <?php endforeach; ?>
+								<div class="form-group">
+									<label for="message">Message</label>
+									<textarea name="message" id="message" class="form-control"></textarea>
+								</div><!-- /form-group -->
+								<input type="hidden" name="author" id="author" value="<?php echo $post['author']?>">
+								<button type="submit" class="btn btn-primary">Add comment</button>
+							</form><!-- /col-lg-6 -->
+						</div><!-- /commentRow -->
+						</div>
+							</div><!--card-body-->
+					</div><!--"card text-white bg-primary mb-3"-->
+				</div><!--"col-10">-->
+			<?php endforeach; ?>
+		</div><!--commentRow-->
 
-			</div>
-		</div>
+
+	</div><!--<div class="container">-->
 	</main>
-
-    </body>
+</body>
 </html>
