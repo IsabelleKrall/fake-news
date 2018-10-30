a63272bdf07847ff31380a6dc1fb27da93f0d15f
Isabelle Krall <isabellekrall@Isabelles-MacBook-Pro.local>
2018-10-30 14:10:20 +0100

    updated formatting

--- head.php
+++ head.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		  <link rel="stylesheet" href="CSS/bootswatch.css">
-		   <link rel="stylesheet" href="CSS/style.css">
-		  <title>Fake News by Fake Authors</title>
-    </head>
-    <body>
-		 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="index.php">Fake News</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-	 <span class="navbar-toggler-icon"></span>
-  </button>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="CSS/bootswatch.css">
+	<link rel="stylesheet" href="CSS/style.css">
+	<title>Fake News by Fake Authors</title>
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+		<a class="navbar-brand" href="index.php">Fake News</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-  <div class="collapse navbar-collapse" id="navbarColor01">
-	 <ul class="navbar-nav mr-auto">
-	   <li class="nav-item">
-		  <a class="nav-link" href="about.php">About</a>
-	   </li>
-	 </ul>
-  </div>
-</nav>
+		<div class="collapse navbar-collapse" id="navbarColor01">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="about.php">About</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
