2c095925afff3c80f21de796bb6c000ba25a8076
Isabelle Krall <isabellekrall@Isabelles-MacBook-Pro.local>
2018-10-31 14:01:14 +0100

    changed to About Fake News

--- about.php
+++ about.php
@@ -5,7 +5,7 @@ require __DIR__.'/head.php';
 ?>
 
 <div class="jumbotron jumbotronBox">
-	<h1 class="display-3">About us</h1>
+	<h1 class="display-3">About Fake News</h1>
 	<p class="lead">Fake News is a site where you can find fake news articles, written by fictional authors.</p>
 	<hr class="my-4">
 	<p>Feel free to read and comment on our articles! We love fake comments as much as we adore fake news.</p>
